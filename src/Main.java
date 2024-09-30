8c00e8b78c75569de48e8becd061bb27710bee09
BrycenP <perezb3@mail.uc.edu>
2024-09-30 15:42:12 -0400

    Completed

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        double winter = 237.35;
+        double spring = 304.61;
+        double summer = 259.10;
+        double fall = 210.27;
+        double year = 0;
+        year = winter + spring + summer + fall;
+        System.out.println("Your total maintenance cost for the year for Winter (" + winter + ") Spring (" + spring + ") Summer (" + summer + ") and Fall (" + fall + ") is " +year);
+    }
+}
